194f201dc4071814b4afb8a8bebf7c8726c94dfe
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-09-21 00:04:15 +0100

    Проверка ДЗ | 6. Условные операторы
    - Правильно: №5, №7, №9, №10
    - Неправильно: №3, №6, №8,
    - Улучшены: №3, №6, №8
    - На доработку:

--- tasks/task_6.py
+++ tasks/task_6.py
@@ -3,9 +3,12 @@ number_month: int = int(input('Введите номер месяца: '))
 
 months: list = [1, 3, 5, 7, 8, 10, 12]
 
-is_leap_year: bool = year % 4 == 0 and year % 100 == 0 or year % 400 == 0
+# Неправильно решено, год не должен быть кратен 100
+# Исправлено!
+is_leap_year: bool = year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
-if number_month in months and is_leap_year == True:
+# is_leap_year == True можно заменить, просто is_leap_year
+if (number_month in months) and is_leap_year:
     print('Да')
 else:
     print('Нет')
